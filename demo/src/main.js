cf80b049490ab7ec6a7323815da9add0e9c71adc
Yuji Sugiura <y.sugiura.0316@gmail.com>
2024-08-16 11:31:06 +0900

    Fixes #1

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,6 +1,21 @@
 import App from "./app.svelte";
-
 import "./main.css";
+
+if (!window.requestIdleCallback) {
+  // @ts-ignore: Polyfill for Safari
+  window.requestIdleCallback = (cb) => {
+    const start = Date.now();
+    return setTimeout(() => {
+      cb({
+        didTimeout: false,
+        timeRemaining: () => Math.max(0, 50 - (Date.now() - start)),
+      });
+    }, 1);
+  };
+
+  window.cancelIdleCallback = (id) => clearTimeout(id);
+}
+
 const app = new App({ target: document.body });
 
 export default app;
